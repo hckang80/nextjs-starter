e8954ce9e232d575353b05fc6b08986b2f5e39b0
hckang80 <hckang80@gmail.com>
2025-08-16 22:15:56 +0900

    style: 디자인 시스템 설치

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import { Theme } from '@radix-ui/themes';
+import '@radix-ui/themes/styles.css';
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
@@ -24,7 +26,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>{children}</body>
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <Theme>{children}</Theme>
+      </body>
     </html>
   );
 }
