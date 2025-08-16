1e427a816ad5c0faab41553537ebde266284df79
hckang80 <hckang80@gmail.com>
2025-08-16 22:56:29 +0900

    fix: fix key name

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,7 +12,7 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
   {
-    ignore: ['node_modules', '.next', 'dist', 'coverage', 'public', '*.config.js', '*.config.mjs']
+    ignores: ['node_modules', '.next', 'dist', 'coverage', 'public', '*.config.js', '*.config.mjs']
   }
 ];
 
