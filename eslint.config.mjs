52382acf57adef9f7d6575c67c8508b7411143bc
hckang80 <hckang80@gmail.com>
2025-08-16 23:24:10 +0900

    chore: 설정 파일 업데이트

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,9 +10,12 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  ...compat.extends('next/core-web-vitals', 'next/typescript', 'next', 'prettier'),
   {
-    ignores: ['node_modules', '.next', 'dist', 'coverage', 'public', '*.config.js', '*.config.mjs']
+    ignores: ['node_modules', '.next', 'dist', 'coverage', 'public', '*.config.js', '*.config.mjs'],
+    rules: {
+      '@next/next/no-img-element': 'warn'
+    }
   }
 ];
 
