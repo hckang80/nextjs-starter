5c68af258753065f587aa83bcdcff337ea25df8d
hckang80 <hckang80@gmail.com>
2025-08-16 22:52:10 +0900

    chore: eslint 설정 업데이트

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,19 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  baseDirectory: __dirname
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    ignore: ['node_modules', '.next', 'dist', 'coverage', 'public', '*.config.js', '*.config.mjs']
+  }
 ];
 
 export default eslintConfig;
