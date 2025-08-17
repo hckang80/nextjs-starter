36e5ce9f3e7fa75f34b2e47f91a0bd982791ce0b
hckang80 <hckang80@gmail.com>
2025-08-17 19:26:10 +0900

    feat: 비로그인 허용 경로 추가

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -1,6 +1,8 @@
 import { createServerClient } from '@supabase/ssr';
 import { NextResponse, type NextRequest } from 'next/server';
 
+const publicPaths = ['/', '/login', '/auth', '/error'];
+
 export async function updateSession(request: NextRequest) {
   let supabaseResponse = NextResponse.next({
     request
@@ -37,13 +39,9 @@ export async function updateSession(request: NextRequest) {
     data: { user }
   } = await supabase.auth.getUser();
 
-  if (
-    !user &&
-    !request.nextUrl.pathname.startsWith('/login') &&
-    !request.nextUrl.pathname.startsWith('/auth') &&
-    !request.nextUrl.pathname.startsWith('/error')
-  ) {
-    // no user, potentially respond by redirecting the user to the login page
+  const isPublicPath = publicPaths.includes(request.nextUrl.pathname);
+
+  if (!user && !isPublicPath) {
     const url = request.nextUrl.clone();
     url.pathname = '/login';
     return NextResponse.redirect(url);
