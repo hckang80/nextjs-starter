90cdb4f4e435b9fa1cc2066e054e63f73e655175
hckang80 <hckang80@gmail.com>
2025-08-17 23:45:31 +0900

    style: 프리티어 룰 개선

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,16 +3,16 @@ import styles from './page.module.css';
 
 export default function LoginPage() {
   return (
-    <Grid align="center" className={styles.root} p="5">
-      <Card size="3">
-        <Text as="div" size="6" weight="bold" mb="2">
+    <Grid align='center' className={styles.root} p='5'>
+      <Card size='3'>
+        <Text as='div' size='6' weight='bold' mb='2'>
           Login
         </Text>
-        <Text as="div" color="gray" size="2">
+        <Text as='div' color='gray' size='2'>
           This demo uses Google for authentication.
         </Text>
 
-        <Button mt="5" color="tomato" size="3" className={styles.button}>
+        <Button mt='5' color='tomato' size='3' className={styles.button}>
           Google
         </Button>
       </Card>
