eeb08567bab963e8c86f9886a7c7f912dc7a6b86
hckang80 <hckang80@gmail.com>
2025-08-17 21:51:58 +0900

    style: 로그인 화면 스타일링

---
+++ src/app/login/page.tsx
@@ -0,0 +1,21 @@
+import { Button, Card, Grid, Text } from '@radix-ui/themes';
+import styles from './page.module.css';
+
+export default function LoginPage() {
+  return (
+    <Grid align="center" className={styles.root} p="5">
+      <Card size="3">
+        <Text as="div" size="6" weight="bold" mb="2">
+          Login
+        </Text>
+        <Text as="div" color="gray" size="2">
+          This demo uses Google for authentication.
+        </Text>
+
+        <Button mt="5" color="tomato" size="3" className={styles.button}>
+          Google
+        </Button>
+      </Card>
+    </Grid>
+  );
+}
